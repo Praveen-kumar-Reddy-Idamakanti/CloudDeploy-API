25fb3a69de686c8f72dc8aaea629900b0a297243
praveen <ageabove60@gmail.com>
2025-12-03 22:26:09 +0530

    changed schema

--- src/config/db.js
+++ src/config/db.js
@@ -31,7 +31,7 @@ const initializeDB = () => {
       `CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL,
-        email TEXT UNIQUE NOT NULL,
+        email TEXT NOT NULL,
         password TEXT NOT NULL,
         createdAt DATETIME DEFAULT CURRENT_TIMESTAMP
       )`,
